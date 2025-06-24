5a9a4cf49440cfb8f1a1f508bee96196b7b9de5c
lmj2352 <minjae.lee@thinkhei.com>
2025-06-24 13:55:06 +0900

    chore: remove environment variable logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,8 @@ import { UserModule } from './user/user.module';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false, // SSL 설정 추가
+      ssl:
+        process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false, // SSL 설정 추가
     }),
     AuthModule,
     SpaceEngineersModule,
@@ -35,21 +36,5 @@ import { UserModule } from './user/user.module';
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggingMiddleware).forRoutes('*');
-  }
-
-  constructor() {
-    console.log('Environment Variables:', {
-      DB_HOST: process.env.DB_HOST,
-      DB_PORT: process.env.DB_PORT,
-      DB_USER: process.env.DB_USER,
-      DB_PASSWORD: process.env.DB_PASSWORD,
-      DB_NAME: process.env.DB_NAME,
-      PORT: process.env.PORT,
-      HOST: process.env.HOST,
-      STEAM_API_KEY: process.env.STEAM_API_KEY,
-      RETURN_URL: process.env.RETURN_URL,
-      REALM: process.env.REALM,
-      JWT_SECRET: process.env.JWT_SECRET,
-    });
   }
 }
