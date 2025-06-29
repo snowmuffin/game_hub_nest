f250effd5cbe7c1b8452a876958cce0cd66cdaa3
Snowmuffin <77647785+snowmuffin@users.noreply.github.com>
2025-06-29 22:22:35 +0900

    fix valheim

--- src/Valheim/building/valheim-building.module.ts
+++ src/Valheim/building/valheim-building.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 import { ValheimBuilding } from './valheim-building.entity';
 import { ValheimBuildingService } from './valheim-building.service';
 import { ValheimBuildingController } from './valheim-building.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ValheimBuilding])],
+  imports: [
+    TypeOrmModule.forFeature([ValheimBuilding]),
+    JwtModule,
+    ConfigModule,
+  ],
   controllers: [ValheimBuildingController],
   providers: [ValheimBuildingService],
   exports: [ValheimBuildingService],
--- src/Valheim/inventory/valheim-inventory.module.ts
+++ src/Valheim/inventory/valheim-inventory.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 import { ValheimInventoryController } from './valheim-inventory.controller';
 import { ValheimInventoryService } from './valheim-inventory.service';
 import { ValheimInventory } from './valheim-inventory.entity';
@@ -7,7 +9,11 @@ import { ValheimItem } from '../item/valheim-item.entity';
 import { User } from '../../user/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ValheimInventory, ValheimItem, User])],
+  imports: [
+    TypeOrmModule.forFeature([ValheimInventory, ValheimItem, User]),
+    JwtModule,
+    ConfigModule,
+  ],
   controllers: [ValheimInventoryController],
   providers: [ValheimInventoryService],
   exports: [ValheimInventoryService],
