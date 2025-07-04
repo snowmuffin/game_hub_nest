8ee56bd043e367fc7fcad170a3ae34484165edca
lmj2352 <minjae.lee@thinkhei.com>
2025-07-04 19:06:03 +0900

    Revert "cleanup migrations"
    This reverts commit 2ca953af993b894f936c12107d56440cbda6c3ba.

---
+++ src/migrations/20250413000100-UpdateOnlineStorageIdToBigInt.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class UpdateOnlineStorageIdToBigInt20250413000100 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE online_storage
+      ALTER COLUMN id TYPE bigint
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE online_storage
+      ALTER COLUMN id TYPE integer
+    `);
+  }
+}
