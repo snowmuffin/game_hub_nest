8f52078ce579c232a4662c6f7a85dca2b95d3a5d
snowmuffin <snowmuffin@users.noreply.github.com>
2025-09-14 13:08:40 +0900

    adjust

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { User } from 'src/entities/shared/user.entity';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET || 'defaultSecret',
+        signOptions: { expiresIn: '6h' },
+      }),
+    }),
+    ConfigModule,
+  ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, JwtAuthGuard],
   exports: [UserService],
 })
 export class UserModule {}
